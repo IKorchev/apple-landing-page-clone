460c8bc21d746b20ddbb438b4e06acee563e9bb7
Korchev <Korchev94@gmail.com>
2021-03-19 17:19:20 +0000

    fixed navbar and made it responsive with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const navToggler = document.querySelector(".navbar-toggler")
 const lines = document.querySelectorAll(".line")
-
-navToggler.addEventListener("click", (e) => {
+const body = document.querySelector("body")
+const collapseContent = document.querySelector("#collapseMe")
+const myCollapse = document.getElementById("collapseMe")
+const bsCollapse = new bootstrap.Collapse(myCollapse, {
+  toggle: false,
+})
+const toggleCollapse = (e) => {
   lines[0].classList.toggle("rotate1")
   lines[1].classList.toggle("rotate2")
-})
+  body.classList.toggle("overflow-hidden")
+  window.scrollTo(0, 0)
+}
+navToggler.addEventListener("click", toggleCollapse)
+
+const checkWindowSize = () => {
+  if (window.innerWidth > 1200) {
+    bsCollapse.hide()
+    lines[0].classList.remove("rotate1")
+    lines[1].classList.remove("rotate2")
+    body.classList.remove("overflow-hidden")
+  }
+}
+
+window.onresize = checkWindowSize // call to determine how big the screen is in order to collapse the navbar
