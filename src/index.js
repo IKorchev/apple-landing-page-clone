55f40d0841537a36ebd0d55f4910e7438a4121d9
Korchev <Korchev94@gmail.com>
2021-03-19 01:14:02 +0000

    some changes to the navbar and more

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const navToggler = document.querySelector(".navbar-toggler")
 const lines = document.querySelectorAll(".line")
 
 navToggler.addEventListener("click", (e) => {
-  e.preventDefault()
   lines[0].classList.toggle("rotate1")
   lines[1].classList.toggle("rotate2")
 })
